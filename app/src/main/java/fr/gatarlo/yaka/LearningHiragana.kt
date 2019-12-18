be43f821ea1fbbbd3f66c85c73a97d509a9c9da7
Gatarlo <44266173+Gatarlo@users.noreply.github.com>
2019-12-18 11:10:09 +0100

    Suppression HiraganaAdapter.kt Création de l'Activité activity_edit_hiragana_line.xml et de sa classe associée EditHiraganaLine.kt Ajout de l'activité dans le xml Les fonctions de gestion d'HiraganaAdapter.kt transférées dans LearningHiragana.kt

--- app/src/main/java/fr/gatarlo/yaka/LearningHiragana.kt
+++ app/src/main/java/fr/gatarlo/yaka/LearningHiragana.kt
@@ -1,9 +1,17 @@
 package fr.gatarlo.yaka
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_learning_hiragana.*
+import kotlinx.android.synthetic.main.cell_hiragana.view.*
 
 class LearningHiragana : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +20,84 @@ class LearningHiragana : AppCompatActivity() {
 
         learningHiraganaRecyclorView.adapter = HiraganaAdapter()
         learningHiraganaRecyclorView.layoutManager = LinearLayoutManager(this)
+    }
+
+    inner class HiraganaAdapter : RecyclerView.Adapter<HiraganaAdapter.HiraganaViewHolder>() {
+
+        val _kanaList = arrayOf(Hiragana("あ\na", "い\ni", "う\nu", "え\ne", "お\no"),
+            Hiragana("か\nka", "き\nki", "く\nku", "け\nke", "こ\nko"),
+            Hiragana("さ\nsa", "し\nshi", "す\nsu", "せ\nse", "そ\nso"),
+            Hiragana("た\nta", "ち\nchi", "つ\ntsu", "て\nte", "と\nto"),
+            Hiragana("な\nna", "に\nni", "ぬ\nnu", "ね\nne", "の\nno"),
+            Hiragana("は\nha", "ひ\nhi", "ふ\nfu", "へ\nhe", "ほ\nho"),
+            Hiragana("ま\nma", "み\nmi", "む\nmu", "め\nme", "も\nmo"),
+            Hiragana("や\nya", "", "ゆ\nyu", "", "よ\nyo"),
+            Hiragana("ら\nra", "り\n　ri", "る\nru", "れ\nre", "ろ\nro"),
+            Hiragana("わ\nwa", "", "", "", "を\nwo"),
+            Hiragana("ん\nn", "", "", "", "")
+        )
+
+        fun onHiraganaClick(index:Int, context: Context) {
+            val hiragana = _kanaList[index]
+
+            val hiraganaIntent = Intent(context, EditHiraganaLine::class.java)
+            hiraganaIntent.putExtra("A_Hiragana", hiragana.A_Hiragana)
+            hiraganaIntent.putExtra("I_Hiragana", hiragana.I_Hiragana)
+            hiraganaIntent.putExtra("U_Hiragana", hiragana.U_Hiragana)
+            hiraganaIntent.putExtra("E_Hiragana", hiragana.E_Hiragana)
+            hiraganaIntent.putExtra("O_Hiragana", hiragana.O_Hiragana)
+            startActivity(hiraganaIntent)
+        }
+
+        inner class HiraganaViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView), View.OnClickListener {
+            val learningHiraganaAKanaDisplay = rootView.A_Hiragana
+            val learningHiraganaIKanaDisplay = rootView.I_Hiragana
+            val learningHiraganaUKanaDisplay = rootView.U_Hiragana
+            val learningHiraganaEKanaDisplay = rootView.E_Hiragana
+            val learningHiraganaOKanaDisplay = rootView.O_Hiragana
+
+            init {
+                rootView.setOnClickListener(this)
+            }
+
+            fun fillWithHiragana (hiragana: Hiragana) {
+                learningHiraganaAKanaDisplay.text = hiragana.A_Hiragana
+                learningHiraganaIKanaDisplay.text = hiragana.I_Hiragana
+                learningHiraganaUKanaDisplay.text = hiragana.U_Hiragana
+                learningHiraganaEKanaDisplay.text = hiragana.E_Hiragana
+                learningHiraganaOKanaDisplay.text = hiragana.O_Hiragana
+            }
+
+            override fun onClick(p0: View?) {
+                if (p0 != null) {
+                    onHiraganaClick(adapterPosition, p0.context)
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HiraganaViewHolder {
+            // 1 — Charger la vue en XML
+            val rootView = LayoutInflater.from(parent.context).inflate(R.layout.cell_hiragana, parent, false)
+
+            // 2 — Créer un viewholder pour contrôler cette vue
+            val holder = HiraganaViewHolder(rootView)
+
+            // 3 — retourner le viewholder
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+            return _kanaList.size
+        }
+
+        override fun onBindViewHolder(holder: HiraganaViewHolder, position: Int) {
+            // 1 — Obtenir le kana
+            val hiragana = _kanaList[position]
+
+            // 2 — Envoyer les infos du kana dans le holder
+            holder.fillWithHiragana(hiragana)
+
+        }
     }
 
 }
